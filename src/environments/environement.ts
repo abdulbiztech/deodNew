9a8c0a48896ea28c6327140538d8601a678cb6d4
abdulbiztech <abdulbiztech@gmail.com>
2023-06-24 21:23:28 +0530

    vercel 3

--- src/environments/environement.ts
+++ src/environments/environement.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:5000',
-  apiUrl:'https://13.232.132.38:5000',
+  apiUrl:'http://13.232.132.38:5000',
   razorpayKey:'rzp_test_rctEhk9DkJO7hU'
 };
