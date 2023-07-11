a92183d1e0beeaa23564b9ae9d41fb03b2a353cd
abdulbiztech <abdulbiztech@gmail.com>
2023-07-11 15:25:47 +0530

    new added

--- src/environments/environement.ts
+++ src/environments/environement.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:5000',
-  // apiUrl:'http://13.232.195.137:5000',
-  apiUrl:"http://207.244.225.171:5000",
+  apiUrl:'http://13.232.195.137:5000',
+  // apiUrl:"http://207.244.225.171:5000",
   // apiUrl: 'http://192.168.1.54:5000',
   razorpayKey:'rzp_test_rctEhk9DkJO7hU'
 };
