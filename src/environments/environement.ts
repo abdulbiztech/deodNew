d721daf58d0a454d1756e91140c8a85d9565a775
abdulbiztech <abdulbiztech@gmail.com>
2024-04-08 16:54:35 +0530

    url changes

--- src/environments/environement.ts
+++ src/environments/environement.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8080',
   apiUrl: 'https://modelbackend.dewood3d.com/',
-  // apiUrl: 'http://localhost:8080',
   // apiUrl :'https://backend.dewood3d.com',
   // apiUrl:'http://13.232.195.137:5000',
   // apiUrl:"http://207.244.225.171:5000",
